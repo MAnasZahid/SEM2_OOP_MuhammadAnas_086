503badcd767a49a2f27ca06ff4938b17f79da778
MAnasZahid <az843666@gmail.com>
2026-08-18 22:39:28 +0500

    Update Calculator.java

--- Week_02(MuhammadAnas)_(086)/CalculatorDemo/Calculator.java
+++ Week_02(MuhammadAnas)_(086)/CalculatorDemo/Calculator.java
@@ -1,32 +1,69 @@
 public class Calculator{
     private double num1;
     private double num2;
+    private static int objectCount = 0;
+    private double result;
+ 
 
+// NULL
+    public Calculator() {
+        this.num1=0;
+        this.num2=0;
+        objectCount++;
+    }
+    // PARAMETERIZED
     public Calculator(double num1, double num2) {
         this.num1 = num1;
         this.num2 = num2;
+        this.result=0;
+        objectCount++;
     }
+    // COPY
+    public  Calculator(Calculator obj) {
+        this.num1=obj.num1;
+        this.num2=obj.num2;
+        this.result=0;
+        objectCount++;
+    }
+// OBJ COUNT
+      public static int ObjectCount() {
+        return objectCount;
+    }
+// GETTERS AND SETTERS
+     double getNum1(){
+        return this.num1;
+    }
+
+    double getNum2(){
+        return this.num2;
+    }
+
+    void setNum1Num2(double num1,double num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+
     public double add() {
-        return num1 + num2;
+        return this.result=num1 + num2;
     }
     public double subtract() {
-        return num1 - num2;
+        return this.result=num1 - num2;
     }
     public double multiply() {
-        return num1 * num2;
+        return this.result=num1 * num2;
     }
     public double divide() {
-        if (num2 != 0) {
-            return num1 / num2;
-        } else {
-            throw new ArithmeticException("Division by zero is not allowed");
-            
-            
-        }
+        if (num2 == 0) {
+             System.out.println("Division by zero is not allowed.");
+        } 
+           
+        return this.result=num1 / num2;
+        
+        
     }
     public double modulus() {
         if (num2 != 0) {
-            return num1 % num2;
+            return this.result=num1 % num2;
         } else {
             throw new ArithmeticException("Modulus by zero is not allowed");
            
@@ -34,60 +71,20 @@ public class Calculator{
     }
     
     public double average() {
-        return (num1 + num2) /2;
-    }
-    
-
-        public void showInfo() {
-            System.out.println("Number 1: " + num1);
-            System.out.println("Number 2: " + num2);  
-            System.out.println("X-----------------------------------------------X");
-            System.out.println();  
+        return this.result=(num1 + num2) /2;
     }
 
 
-    public void showadd() {
-            System.out.println("Addition: " + add());
-            System.out.println("X-----------------------------------------------X");
-            System.out.println();
+     public String toString() {
+        return ("Num 1:"+num1+"\nNum 2:"+num2+"\ncalculate :"+this.result+"\nobject count:"+Calculator.ObjectCount());
     }
 
-     public void showsub() {
-            System.out.println("Subtraction: " + subtract());
-            System.out.println("X-----------------------------------------------X");
-            System.out.println();
+    //show consructor
+    void show() {
+        System.out.println(this.toString());
+        System.out.println("X-----------------------------------------------X");
+        System.out.println();
     }
 
-     public void showmulti() {
-            System.out.println("Multiplication: " + multiply());
-            System.out.println("X-----------------------------------------------X");
-            System.out.println();
-    }
-
-     public void showdiv() {
-        try {
-            System.out.println("Division: " + divide());
-            System.out.println("X-----------------------------------------------X");
-            System.out.println();
-        } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-     public void showmod() {
-        try {
-            System.out.println("Modulus: " + modulus());
-            System.out.println("X-----------------------------------------------X");
-            System.out.println();
-        } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-     public void showavg() {
-            System.out.println("Average: " + average());
-            System.out.println("X-----------------------------------------------X");
-            System.out.println();
-    }
 
 }
