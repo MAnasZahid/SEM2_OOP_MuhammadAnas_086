266858d6a1bfe6ef53d2e34195cd386a320ea6ee
MAnasZahid <az843666@gmail.com>
2026-08-18 20:23:30 +0500

    Update Point.java

--- Week03(MuhammadAnas)_(086)/Point.java
+++ Week03(MuhammadAnas)_(086)/Point.java
@@ -6,6 +6,7 @@ public class Point {
 
     public Point() {
         this(0, 0);
+        
     }
 
     public Point(double xCoord, double yCoord) {
@@ -16,6 +17,7 @@ public class Point {
 
     public Point(Point source) {
         this(source.xCoord, source.yCoord);
+        
     }
 
     public double getX() {
@@ -30,7 +32,7 @@ public class Point {
         this.xCoord = xCoord;
     }
 
-    public void setY(double yCoord) {
+    public void setY(double  yCoord) {
         this.yCoord = yCoord;
     }
 
@@ -39,30 +41,45 @@ public class Point {
         this.yCoord = yCoord;
     }
 
-    public Point plus(Point other) {
-        return new Point(this.xCoord + other.xCoord, this.yCoord + other.yCoord);
+    public Point plus(Point obj) {
+        return new Point(this.xCoord + obj.xCoord, this.yCoord + obj.yCoord);
     }
 
-    public Point minus(Point other) {
-        return new Point(this.xCoord - other.xCoord, this.yCoord - other.yCoord);
+    public Point minus(Point obj) {
+        return new Point(this.xCoord - obj.xCoord, this.yCoord - obj.yCoord);
     }
 
-    public double distanceTo(Point other) {
-        double dx = this.xCoord - other.xCoord;
-        double dy = this.yCoord - other.yCoord;
-        return Math.sqrt(dx * dx + dy * dy);
+    double distanceTo(Point obj) {
+        return Math.sqrt(Math.pow(this.xCoord-obj.xCoord,2) + Math.pow(this.yCoord-obj.yCoord,2));
     }
 
-    public boolean equals(Point other) {
-        return this.xCoord == other.xCoord && this.yCoord == other.yCoord;
+    public boolean equals(Point obj) {
+        return this.xCoord == obj.xCoord && this.yCoord == obj.yCoord;
+    
     }
+     Point midPoint(Point obj){
+        return new Point((this.xCoord+obj.xCoord)/2 ,(this.yCoord+obj.yCoord)/2);
+     }
 
     public static int totalCreated() {
         return totalPoints;
     }
 
+     double slope(Point obj){
+        if(this.xCoord!=obj.xCoord)
+            return (this.xCoord-obj.xCoord)/(this.yCoord-obj.yCoord);
+        else 
+            return Double.POSITIVE_INFINITY;
+    }
+
 
     public String toString() {
-        return "(" + xCoord + ", " + yCoord + ")";
+        return String.format("x is: "+this.xCoord+" y is: "+this.yCoord+ "  object count is: "+Point.totalCreated());
     }
+
+
+    public void show(){
+        System.out.println(toString());
+        System.out.println("-------");
+}
 }
